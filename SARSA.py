bcfe2cd4494797283741d3f0dc8e1c850c902bda
S.T. Ketema <s4184343@u0065078.vuw.leidenuniv.nl>
2026-03-07 02:45:10 +0100

    SARSA implemented

--- SARSA.py
+++ SARSA.py
@@ -13,11 +13,17 @@ from Agent import BaseAgent
 class SarsaAgent(BaseAgent):
         
     def update(self,s,a,r,s_next,a_next,done):
-        # TO DO: Add own code
-        pass
+        if not done:
+            G_t = r + self.gamma * (self.Q_sa[s_next,a_next])
+            self.Q_sa[s,a] = self.Q_sa[s,a] + self.learning_rate * (G_t - self.Q_sa[s,a])       
+        else: 
+            G_t = r 
+            self.Q_sa[s,a] = self.Q_sa[s,a] + self.learning_rate * (G_t - self.Q_sa[s,a])      
+        
+        
 
         
-def sarsa(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True, eval_interval=500):
+def sarsa(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True, eval_interval=1000):
     ''' runs a single repetition of SARSA
     Return: rewards, a vector with the observed rewards at each timestep ''' 
     
@@ -28,7 +34,29 @@ def sarsa(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, tem
     eval_returns = []
 
     # TO DO: Write your SARSA algorithm here!
+    done = False
+    steps = 0
+    # sample initial state
     
+    s = env.reset()
+    while steps < n_timesteps:
+        a = pi.select_action(s,epsilon=epsilon,policy='egreedy')
+        s_next,r,done = env.step(a)
+        a_next = pi.select_action(s_next,epsilon=epsilon,policy='egreedy')
+        pi.update(s, a= a, r=r, s_next=s_next, a_next=a_next,done=done)
+        if done:
+            s = env.reset()
+            
+            
+        else:
+            s = s_next
+            
+        
+        steps += 1
+        if steps % eval_interval == 0:
+            eval_ret = pi.evaluate(eval_env=eval_env)
+            eval_timesteps.append(steps)
+            eval_returns.append(eval_ret)
     # if plot:
     #    env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during SARSA execution
 
@@ -36,7 +64,7 @@ def sarsa(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, tem
 
 
 def test():
-    n_timesteps = 1000
+    n_timesteps = 50001
     gamma = 1.0
     learning_rate = 0.1
 
@@ -47,8 +75,9 @@ def test():
     
     # Plotting parameters
     plot = True
-    sarsa(n_timesteps, learning_rate, gamma, policy, epsilon, temp, plot)
-            
+    eval_returns, eval_timesteps = sarsa(n_timesteps, learning_rate, gamma, policy, epsilon, temp, plot)
+    for i in range(len(eval_timesteps)):
+        print(f"Timestep {eval_timesteps[i]} = {eval_returns[i]} mean return ")        
     
 if __name__ == '__main__':
     test()
